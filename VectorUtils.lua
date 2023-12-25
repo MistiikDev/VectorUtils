bd00a0262c19703415f1ee859df904ffead93b9b
DAF <91028158+MistiikDev@users.noreply.github.com>
2023-12-25 21:03:48 +0100

    Create VectorUtils.lua

---
+++ VectorUtils.lua
@@ -0,0 +1,15 @@
+local VectorUtils = {}
+
+function VectorUtils.GetPiercingVector(part : BasePart, _incidentVector : Vector3, _normal : Vector3, _axis : string)
+	local _computedVectorB = Vector3.new()
+	local width = part.Size[_axis]
+	
+	local theta = math.acos(-_normal.Unit:Dot(_incidentVector))
+	local offset = math.tan(theta) * width
+	
+	_computedVectorB = (_incidentVector + offset).Unit
+	
+	return _computedVectorB
+end
+
+return VectorUtils
